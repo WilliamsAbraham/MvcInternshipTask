4664018d7724a80c268bbc4800e755ed46e3ae52
Abraham <68301663+WilliamsAbraham@users.noreply.github.com>
2022-09-13 17:02:22 +0100

    update

---
+++ PosApp/pos/Controllers/CardController.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace pos.Controllers
+{
+    public class CardController : Controller
+    {
+        // GET: Card
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // GET: Card/Details/5
+        public ActionResult CardDetails(int id)
+        {
+            return View();
+        }
+
+        // GET: Card/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Card/Create
+        [HttpPost]
+        public ActionResult Create(FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Card/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: Card/Edit/5
+        [HttpPost]
+        public ActionResult Edit(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Card/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: Card/Delete/5
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
