43f608291dd31ce44911de987e823b745c3eb798
Abraham <68301663+WilliamsAbraham@users.noreply.github.com>
2022-10-04 02:23:05 +0100

    last commits

---
+++ PosApp/PosApp/Models/RceiptViewModel.cs
@@ -0,0 +1,11 @@
+﻿namespace PosApp.Models
+{
+    public class RceiptViewModel
+    {
+        public MerchantDetails merchant { get; set; }
+        public PurchaseDetails purchase { get; set; }
+        public CardDetails card { get; set; }
+        public TransactionDetails transaction { get; set; }
+
+    }
+}
